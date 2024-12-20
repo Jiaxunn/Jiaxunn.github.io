2919ae8117a5c34f46a16bf8455a645cbfb45030
jl1099 <jl1099@duke.edu>
2024-12-20 16:46:25 -0500

    update

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 function toggleMenu() {
   const menu = document.querySelector(".menu-links");
   const icon = document.querySelector(".hamburger-icon");
+  const profileSection = document.getElementById('profile');
+  profileSection.classList.toggle('profile-shifted');
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
